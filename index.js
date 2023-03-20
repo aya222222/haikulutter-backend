19def0176c871e4bf9e357673d9887ca9603c87e
aya222222 <80786703+aya222222@users.noreply.github.com>
2023-03-20 19:44:19 -0400

    changed

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.setHeader("Cross-origin-Opener-Policy", "same-origin-allow-popups");
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Cross-origin-Opener-Policy", "same-origin-allow-popups");
+// });
 
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
